6a84a0ebee6d73d313104516a96437fce237be92
AkashPandey8789 <akashpandeyssm71@gmail.com>
2023-10-19 11:32:54 +0530

    First order and higher order func.

--- functions.js
+++ functions.js
@@ -25,3 +25,28 @@ console.log(flight);
 console.log(jonas);//since objects are passed by reference its original value will also change...
 //Note: Javascript doesn't have pass by reference it only has pass by value...
 //when we pass an object we pass it's memory location value and not it acts as reference...
+
+//First-Class functions and Higher Order Functions...
+//Javascript treats functions as First-class citizens
+//that means functions are simply values..
+//functions are just another 'type' of object..
+
+//we store functions in variables..
+
+const adder=(a,b)=>a+b;
+const counter={
+    countVal:23,
+    incrementer:function(){this.countVal++;}
+
+}
+
+//we can pass function into another function's parameter like we do in addEventLister..
+
+//Higher order functions...
+//A function that receives another function as an argument that returns a new function or both.
+//This is only possible because of first-class function
+
+//-----Both of the below functions are            *Higher Order Function*
+//function that receives another function...
+//function that returns a new function 
+
