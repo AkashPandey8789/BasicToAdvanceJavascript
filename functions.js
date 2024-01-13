9d2dceaafe2babcba3ed1181618515ab11b1fdeb
AkashPandey8789 <akashpandeyssm71@gmail.com>
2024-01-13 12:15:40 +0530

    using bind method with functions

--- functions.js
+++ functions.js
@@ -72,6 +72,9 @@ const StudentClass={
     marks:987,
     displayReport(){
         console.log(`${this.name} with roll ${this.Roll} has marks ${this.marks}` );
+    },
+    studentAddressLine(ward,street){
+        console.log(`${this.name} lives at ${ward} , ${street} `);
     }
 }
 
@@ -91,3 +94,57 @@ stObj.call(objStudent);
 console.log(stObj.name); //name will give name of function..
 
 
+//-----------------------bind method--------------------------
+
+//** like call method bind doesn't call the method but it returns a new method with object bound to this keyword of that method...
+
+const studentMethod=stObj.bind(objStudent);
+studentMethod();
+//scenario ==>we can use bind in result creation we can have the grading system constant defined in method and just bind different students object to generate result/report card.
+
+//we can also set some parameters with bind and also this
+
+const studentAddressMethod=StudentClass.studentAddressLine;
+const stdAddress=studentAddressMethod.bind(objStudent,39);
+stdAddress('Kanserwa');
+
+//creating school obj....
+const schoolObj={
+    name:'akps',
+    studentCount:200
+}
+//With Event Listeners
+schoolObj.studentCountIncrementor=function(){
+    console.log(this);
+    this.studentCount++;
+    console.log(this.studentCount);
+}
+//this keyword of the handler function points to the querySelector it is attached to ...
+document.querySelector('.addStudent').addEventListener
+('click',schoolObj.studentCountIncrementor.bind(schoolObj));//here we are using bind method because we don't want the method to get called immediately... and this way we are updating the this keyword for the method...
+//in short bind helps create partial application..
+
+//not only this we can use bind to set other parameters also ,it will help create a whole new method...
+
+//Scenario Tax calculator ...
+const taxForEdibles=(rate,values)=>values+(values*rate);
+
+const taxForSpicyEdibles=taxForEdibles.bind(null,0.2);//first parameter of bind is this keyword , here we are setting it null..
+const taxForSweetEdibles=taxForEdibles.bind(null,0.9);
+const taxForSweetestEdibles=taxForSweetEdibles.bind(null,10);
+console.log(`${taxForSpicyEdibles(20)} spicy`);
+console.log(taxForSweetEdibles(2));
+console.log(taxForSweetestEdibles(2));
+
+
+//creating bind scenario using function returning other function ... using higher order method 
+//below taxForGeneralMaterial method is higher order method
+//taxForSpecificeMaterial is first order method...
+const taxForMaterial=function taxForGeneralMaterial(rates){
+    return function taxForSpecificeMaterial(values){
+        return values+values*rates;
+    }
+}
+
+const taxForMaggii=taxForMaterial(0.2);
+console.log(`10 maggi cost ${taxForMaggii(10)} with taxes`);
