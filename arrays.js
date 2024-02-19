e8a9bc22ee562babc4e9ec727796c9ec41373a6c
AkashPandey8789 <akashpandeyssm71@gmail.com>
2024-02-19 10:01:03 +0530

    array basic methods

---
+++ arrays.js
@@ -0,0 +1,33 @@
+//methods are functions attached to objects.. arrays methods are same.. arrays are object and methods are functions attached to them
+let arr=['a','b','c','d','e'];
+
+//slice
+console.log(arr.slice(2));//slice returns a new array after extracting from existing array.
+console.log(arr.slice(2,4));//end parameter will not be included
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice());//shallow copy of array...
+console.log([...arr]); // shallow copy using spread operator.
+
+//SPLICE
+//-->it works same as slice but it does mutate the original array.
+console.log(arr.splice(2));//elements starting from 2 index will be returned and they will also be removed from original array.
+console.log(arr);//left over elements will be returned from array
+
+
+//REVERSE
+//-->it reverses the elements returs result as array and mutates the original array.
+arr=['a','b','c','d','e','f'];
+console.log(arr.reverse());
+console.log(arr);
+
+
+//CONCAT
+//-->it concats two arrays but doesn't mutate any original array.
+const letters=arr.concat(['g','h']);
+console.log(letters)
+
+//JOIN
+//--> it joins all the elements of array into string by putting character provided between them.
+console.log(letters.join('-'));
+
